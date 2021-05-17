219b1c0b195bbe0587c508255372b820137bcaf3
ShybeeThomas <84278497+ShybeeThomas@users.noreply.github.com>
2021-05-17 20:27:19 +0530

    Add files via upload

---
+++ action.js
@@ -0,0 +1,9 @@
+function green(){
+document.body.style.background="green"
+}
+function yellow(){
+    document.body.style.background="yellow"
+}
+function blue(){
+    document.body.style.background="blue"
+}
